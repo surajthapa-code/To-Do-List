a2fb1e79ebf650689a650a2da38dc208e552397a
Suraj Thapa <surajthapa7787@gmail.com>
2025-10-15 11:43:58 +0530

    added clone function

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
+
 const newButton = document.querySelector(".create-new");
-const main = document.querySelector("#main");
+const main = document.querySelector("#main-section");
 const checkbox = document.querySelector("#custom-checkbox");
 const taskbar = document.querySelector(".task-bar");
 const task = document.querySelector("#text");
 
 newButton.addEventListener("click",()=>{
-    
+    const newtaskbar = taskbar.cloneNode(true);
+    main.appendChild(newtaskbar);
+
 })
 checkbox.addEventListener("change", ()=>{
     taskbar.style.display = "none"
