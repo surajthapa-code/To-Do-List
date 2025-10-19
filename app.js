2a498f7e0bb3642d23fdb8e4f99b17b1d5245604
Suraj Thapa <surajthapa7787@gmail.com>
2025-10-19 19:07:00 +0530

    readded functions

--- app.js
+++ app.js
@@ -5,17 +5,45 @@
     const main = document.querySelector('main');
     const todoApp = document.querySelector('.todo-app');
     const listItems = listContainer.querySelectorAll('li');
+    
+    
+    
+    
 
     //functions -->
     createBtn.addEventListener('click',createnewtask =()=>{
         if(input.value === ''){
             input.placeholder = "enter a valid text";
         }else{
-            input.value = '';
              let li = document.createElement("li");
         li.innerHTML = input.value;
         listContainer.appendChild(li);
+        input.value = '';
+        saveData()
         }
-        console.log("hl")
+        
     })
-    
+    createBtn.addEventListener("click",(evt)=>{
+        evt.preventDefault();
+    })
+
+    listContainer.addEventListener('click',function(e){
+        if(e.target.tagName = "LI"){
+            e.target.classList.toggle("checked")
+            e.target.innerHTML = "(Auto delete in 2 sec)"
+            saveData ()
+              setTimeout(() => {
+                e.target.remove();
+                saveData ()
+        }, 2000);
+        }
+        
+    })
+    //save and show Data trough local storage
+    function saveData (){
+        localStorage.setItem("Data",listContainer.innerHTML);
+    }
+    function showData (){
+        listContainer.innerHTML = localStorage.getItem("Data");
+    }
+    showData();
