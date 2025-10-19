f07eae507494e5c7c917b443764bf7961883291e
Suraj Thapa <surajthapa7787@gmail.com>
2025-10-19 19:01:07 +0530

    ToDo-List done add or remove your task and data will not erase if you refresh the page

--- app.js
+++ app.js
@@ -1,15 +1,21 @@
+    // Basic element selections matching your index.html
+    const input = document.getElementById('input');
+    const createBtn = document.querySelector('.row button');
+    const listContainer = document.querySelector('.list-container');
+    const main = document.querySelector('main');
+    const todoApp = document.querySelector('.todo-app');
+    const listItems = listContainer.querySelectorAll('li');
 
-const newButton = document.querySelector(".create-new");
-const main = document.querySelector("#main-section");
-const checkbox = document.querySelector("#custom-checkbox");
-const taskbar = document.querySelector(".task-bar");
-const task = document.querySelector("#text");
-
-newButton.addEventListener("click",()=>{
-    const newtaskbar = taskbar.cloneNode(true);
-    main.appendChild(newtaskbar);
-
-})
-checkbox.addEventListener("change", ()=>{
-    taskbar.style.display = "none"
-})
+    //functions -->
+    createBtn.addEventListener('click',createnewtask =()=>{
+        if(input.value === ''){
+            input.placeholder = "enter a valid text";
+        }else{
+            input.value = '';
+             let li = document.createElement("li");
+        li.innerHTML = input.value;
+        listContainer.appendChild(li);
+        }
+        console.log("hl")
+    })
+    
